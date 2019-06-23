3665fe868553200f745ba5b004b95c50f313eedd
bartstachurski <bart.stachurski@gmail.com>
2019-06-23 12:55:38 -0400

    rewrite controller to use jb views instead of storing data within controller file

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,17 +1,13 @@
 class Api::ExamplePagesController < ApplicationController
-    def the_good_morning_method
-    render json: {message: "good morning"}
+  def the_good_morning_method
+    render 'good_morning.json.jb'
   end
 
   def hello_world_method
-    render json: {message: "hello world!"}
+    render 'hello_world.json.jb'
   end
 
   def complex_info_method
-    render json: {array: [
-      {first_name: "Bart", last_name: "Stachurski", status: "Rules", cars: ["1992 Volvo 740", "2014 Subaru Outback"]},
-      {first_name: "Keanu", last_name: "Reaves", status: "Breathtaking"}
-    ]
-    }
+    render 'complex_info_method.json.jb'
   end
 end
